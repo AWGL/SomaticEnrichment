7b9c42d50d7f4f29320f250f3311b2c5ccb3e160
transfer <transfer@cvx-gen01.cardiffandvale.wales.nhs.uk>
2018-11-22 17:44:03 +0000

    removeed --ci from calling function in CNVkit

--- lib/2_cnvkit.sh
+++ lib/2_cnvkit.sh
@@ -42,7 +42,7 @@ echo "seqgmentation"
 $cnvkit segment "$odir"/"$test_sample".cnr -m cbs -o "$odir"/"$test_sample".cns --vcf "$odir"/"$test_sample"_common.vcf
 
 echo "CNV calling"
-$cnvkit call "$odir"/"$test_sample".cns -o "$odir"/"$test_sample".call.cns --ci --vcf "$odir"/"$test_sample"_common.vcf
+$cnvkit call "$odir"/"$test_sample".cns -o "$odir"/"$test_sample".call.cns --vcf "$odir"/"$test_sample"_common.vcf
 
 $cnvkit metrics "$test_sample".targetcoverage.cnn "$test_sample".antitargetcoverage.cnn "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".cns > "$odir"/"$test_sample".metrics
 $cnvkit scatter "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".cns -v "$odir"/"$test_sample"_common.vcf -o "$odir"/"$test_sample"-scatter.pdf
