3104693e99324bbec75290a55d8f913ff779a193
transfer <transfer@cvx-gen01.cardiffandvale.wales.nhs.uk>
2019-01-31 12:08:11 +0000

    using call.cns file for genemetics; changes 0t value in genemetics command; using hmm segmentation

--- lib/2_cnvkit.sh
+++ lib/2_cnvkit.sh
@@ -35,15 +35,15 @@ echo "selecting common germline variants for CNV backbone"
     --selectExpressions 'POP_AF < 0.95'
 
 echo "seqgmentation"
-$cnvkit segment "$odir"/"$test_sample".cnr -m cbs -o "$odir"/"$test_sample".cns --vcf "$odir"/"$test_sample"_common.vcf --drop-low-coverage
-$cnvkit segmetrics -s "$odir"/"$test_sample".cn{s,r} --ci --pi
+$cnvkit segment "$odir"/"$test_sample".cnr -m hmm -o "$odir"/"$test_sample".cns --vcf "$odir"/"$test_sample"_common.vcf --drop-low-coverage
+$cnvkit segmetrics -s "$odir"/"$test_sample".cn{s,r} --ci
 
 $cnvkit call "$odir"/"$test_sample".segmetrics.cns -o "$odir"/"$test_sample".call.cns --vcf "$odir"/"$test_sample"_common.vcf -m threshold -t=-0.32,-0.15,0.14,0.26 --filter ci
 
-$cnvkit metrics "$test_sample".targetcoverage.cnn "$test_sample".antitargetcoverage.cnn "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".segmetrics.cns > "$odir"/"$test_sample".metrics
-$cnvkit scatter "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".segmetics.cns -v "$odir"/"$test_sample"_common.vcf -o "$odir"/"$test_sample"-scatter.pdf
-$cnvkit breaks "$odir"/"$test_sample".cnr "$odir"/"$test_sample".segmetics.cns > "$odir"/"$test_sample".breaks
-$cnvkit genemetrics "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".segmetics.cns -m 3 -t 0.14 > "$odir"/"$test_sample".genemetrics
+$cnvkit metrics "$test_sample".targetcoverage.cnn "$test_sample".antitargetcoverage.cnn "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".call.cns > "$odir"/"$test_sample".metrics
+$cnvkit scatter "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".call.cns -v "$odir"/"$test_sample"_common.vcf -o "$odir"/"$test_sample"-scatter.pdf
+$cnvkit breaks "$odir"/"$test_sample".cnr "$odir"/"$test_sample".call.cns > "$odir"/"$test_sample".breaks
+$cnvkit genemetrics "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".call.cns -m 3 -t 0.13 > "$odir"/"$test_sample".genemetrics
 $cnvkit sex "$odir"/"$test_sample".*.cnn "$odir"/"$test_sample".cnr "$odir"/"$test_sample".segmetrics.cns > "$odir"/"$test_sample".sex
 
 # generate CNV report for each panel
