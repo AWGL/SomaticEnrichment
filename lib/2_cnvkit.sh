24a5c1a12d51c48b60426084a6839b563e3ef9fa
transfer <transfer@cvx-gen01.cardiffandvale.wales.nhs.uk>
2019-02-01 11:48:42 +0000

    reinstated cbs for segmentation

--- lib/2_cnvkit.sh
+++ lib/2_cnvkit.sh
@@ -35,7 +35,7 @@ echo "selecting common germline variants for CNV backbone"
     --selectExpressions 'POP_AF < 0.95'
 
 echo "seqgmentation"
-$cnvkit segment "$odir"/"$test_sample".cnr -m hmm -o "$odir"/"$test_sample".cns --vcf "$odir"/"$test_sample"_common.vcf --drop-low-coverage
+$cnvkit segment "$odir"/"$test_sample".cnr -m cbs -o "$odir"/"$test_sample".cns --vcf "$odir"/"$test_sample"_common.vcf --drop-low-coverage
 $cnvkit segmetrics -s "$odir"/"$test_sample".cn{s,r} --ci
 
 $cnvkit call "$test_sample".segmetrics.cns -o "$odir"/"$test_sample".call.cns --vcf "$odir"/"$test_sample"_common.vcf -m threshold -t=-0.32,-0.15,0.14,0.26 --filter ci
@@ -44,6 +44,7 @@ $cnvkit metrics "$test_sample".targetcoverage.cnn "$test_sample".antitargetcover
 $cnvkit scatter "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".call.cns -v "$odir"/"$test_sample"_common.vcf -o "$odir"/"$test_sample"-scatter.pdf
 $cnvkit breaks "$odir"/"$test_sample".cnr "$odir"/"$test_sample".call.cns > "$odir"/"$test_sample".breaks
 $cnvkit genemetrics "$odir"/"$test_sample".cnr -s "$odir"/"$test_sample".call.cns -m 3 -t 0.13 > "$odir"/"$test_sample".genemetrics
+$cnvkit genemetrics "$odir"/"$test_sample".cnr -m 3 -t 0.13 > "$odir"/"$test_sample".unsegmented.genemetrics
 $cnvkit sex "$odir"/"$test_sample".*.cnn "$odir"/"$test_sample".cnr "$odir"/"$test_sample".call.cns > "$odir"/"$test_sample".sex
 
 # generate CNV report for each panel
